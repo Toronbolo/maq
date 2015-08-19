a00363d169d009ca703017f51655d8330c93c761
Toronbolo <yorknew45@gmail.com>
2015-08-19 09:57:57 -0400

    added some drop down lists

--- index.php
+++ index.php
@@ -21,14 +21,14 @@
 		<H1>Reporte de maquinas de costura</H1>
 		<p class="top-p">Por favor llene los campos requeridos para poder realizar el reporte de su maquina</p>
 		
-		<!-- Formularios -->
+	<!-- Formularios -->
+		<!-- Informacion de usuario -->
+		<H4>Informacion de usuario</H4>
 		<form class="form-inline">
 		  <div class="form-group">
 		    <label for="codigo">Codigo</label>
 		    <input type="text" class="form-control" id="codigo" placeholder="21045"> 
 		  </div>
-		  <br />
-		  <br />
 		  <div class="form-group">
 		    <label for="nombre">Nombre</label>
 		    <input type="email" class="form-control" id="nombre" placeholder="Maria">
@@ -38,6 +38,71 @@
 		    <input type="email" class="form-control" id="apellido" placeholder="Polanco">
 		  </div>
 		  <br />
+		  <br />
+		  <label for="drop-down-equipo">Seleccione su equipo</label>
+		  <select name="list-equipo" id="drop-down-equipo">
+		  	<option value="a01">A01</option>
+		  	<option value="a02">A02</option>
+		  	<option value="a03">A03</option>
+		  	<option value="a04">A04</option>
+		  	<option value="a05">A05</option>
+		  	<option value="a06">A06</option>
+		  	<option value="a07">A07</option>
+		  	<option value="a08">A08</option>
+		  	<option value="a09">A09</option>
+		  	<option value="a10">A10</option>
+		  	<option value="a11">A11</option>
+		  </select>
+
+		  <label for="drop-down-supervisor">Seleccione su supervisor</label>
+		  <select name="list-supervisor" id="drop-down-supervisor">
+		  	<option value="a01">A01</option>
+		  	<option value="a02">A02</option>
+		  	<option value="a03">A03</option>
+		  	<option value="a04">A04</option>
+		  	<option value="a05">A05</option>
+		  	<option value="a06">A06</option>
+		  	<option value="a07">A07</option>
+		  	<option value="a08">A08</option>
+		  	<option value="a09">A09</option>
+		  	<option value="a10">A10</option>
+		  	<option value="a11">A11</option>
+		  </select>
+		  <!-- Informacion de usuario -->
+
+		  <!-- Reporte de problema -->
+		  <br />
+		  <br />
+		  <label for="drop-down-operacion">Seleccione la operacion</label>
+		  <select name="list-operacion" id="drop-down-operacion">
+		  	<option value="rc">RC</option>
+		  	<option value="uh">UH</option>
+		  	<option value="hc">HC</option>
+		  	<option value="mc">MC</option>
+		  	<option value="dc">DC</option>
+		  	<option value="dp">DP</option>
+		  	<option value="pv">PV</option>
+		  	<option value="st">ST</option>
+		  	<option value="mm">MM</option>
+		  	<option value="cm">CM</option>
+		  	<option value="rm">RM</option>
+		  </select>
+
+		  <label for="drop-down-problema">Seleccione problema</label>
+		  <select name="list-problema" id="drop-down-problema">
+		  	<option value="salto">Salto</option>
+		  	<option value="rompe hilo">Rompe hilo</option>
+		  	<option value="parte aguja">Parte aguja</option>
+		  	<option value="mc">MC</option>
+		  	<option value="dc">DC</option>
+		  	<option value="dp">DP</option>
+		  	<option value="pv">PV</option>
+		  	<option value="st">ST</option>
+		  	<option value="mm">MM</option>
+		  	<option value="cm">CM</option>
+		  	<option value="rm">RM</option>
+		  </select>
+		  <!-- Reporte de problema -->
 		  <br />
 		  <button type="submit" class="btn btn-default">Enviar</button>
 		</form>
